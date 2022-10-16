d59cc3a0d1b2a53f5661614f29072a2142c27488
Yevgen Yakovliev <yyakovliev02@gmail.com>
2022-10-16 21:14:22 +0300

    feat: changed image versions to 1

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 eval $(minikube docker-env)
-docker build -t warehouse -f services/warehouse/Dockerfile services/warehouse
-docker build -t orders -f services/orders/Dockerfile services/orders
-docker build -t customers -f services/customers/Dockerfile services/customers
-docker build -t auth -f services/auth/Dockerfile services/auth
+docker build -t warehouse:1 -f services/warehouse/Dockerfile services/warehouse
+docker build -t orders:1 -f services/orders/Dockerfile services/orders
+docker build -t customers:1 -f services/customers/Dockerfile services/customers
+docker build -t auth:1 -f services/auth/Dockerfile services/auth
+docker build -t client:1 -f client/Dockerfile client
