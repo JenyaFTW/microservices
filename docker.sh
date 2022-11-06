0a2d1d9c53280da1db59a121d1ad07e198c7b543
Yevgen Yakovliev <yyakovliev02@gmail.com>
2022-11-06 23:03:08 +0200

    feat: added docker migrations to build script

--- docker.sh
+++ docker.sh
@@ -2,5 +2,6 @@ eval $(minikube docker-env)
 docker build -t warehouse:1 -f services/warehouse/Dockerfile services/warehouse
 docker build -t orders:1 -f services/orders/Dockerfile services/orders
 docker build -t customers:1 -f services/customers/Dockerfile services/customers
-docker build -t auth:1 -f services/auth/Dockerfile services/auth
+docker build -t auth-service:2 -f services/auth/Dockerfile services/auth
+docker build -t auth-migrations:2 -f services/auth/migrations/Dockerfile services/auth/migrations
 docker build -t client:1 -f client/Dockerfile client
