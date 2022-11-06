3576c0ae0370bdd425e4148238269ab9b0263d43
maxim <makkov2705@gmail.com>
2022-11-07 01:32:48 +0200

    feat: add migrations to build script
    Revert "feat: add migrations to build script"
    
    This reverts commit 82527ac8fc992b8d2fb8b8a9b5262b3afd90580b.
    
    Auto stash before revert of "feat: add migrations to build script"
    minor: remove unused file

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 eval $(minikube docker-env)
-docker build -t warehouse:1 -f services/warehouse/Dockerfile services/warehouse
+docker build -t warehouse:2 -f services/warehouse/Dockerfile.main services/warehouse
+docker build -t warehouse-migrations:2 -f services/warehouse/Dockerfile.migration services/warehouse
 docker build -t orders:1 -f services/orders/Dockerfile services/orders
 docker build -t customers:1 -f services/customers/Dockerfile services/customers
 docker build -t auth-service:2 -f services/auth/Dockerfile services/auth
