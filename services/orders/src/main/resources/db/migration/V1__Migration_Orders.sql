5a501f9d65e70d5df8341c05dcbe0224171bc2b1
milina-s <samokhatnyaya.milina@gmail.com>
2022-11-12 21:17:45 +0200

    update migration

--- services/orders/src/main/resources/db/migration/V1__Migration_Orders.sql
+++ services/orders/src/main/resources/db/migration/V1__Migration_Orders.sql
@@ -2,17 +2,16 @@ CREATE SCHEMA IF NOT EXISTS orders;
 
 CREATE TABLE IF NOT EXISTS orders.order
 (
-    "id" INT8 NOT NULL,
-    "client_id" INT8,
-    PRIMARY KEY ("id")
-);
+    id INT NOT NULL,
+    client_id INT,
+    PRIMARY KEY (id)
+    );
 
 CREATE TABLE IF NOT EXISTS orders.order_item_id
 (
-    "order_id" INT8 NOT NULL,
-    "item_id" INT8
+    order_id INT NOT NULL,
+    item_id INT
 );
 
 ALTER TABLE IF EXISTS orders.order_item_id
-    ADD FOREIGN KEY ("order_id") REFERENCES "order"
-
+    ADD FOREIGN KEY (order_id) REFERENCES orders.order
